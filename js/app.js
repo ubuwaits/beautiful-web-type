82bd0e3556df913fa9ace897017081bd430491d5
Chad Mazzola <chad.mazzola@gmail.com>
2018-10-16 15:45:04 +0200

    Rewrite mobile menu JS to remove jQuery dependency.

--- js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.menu-link').click(function(e) {
-    e.preventDefault();
-    $('.mobile-menu').toggleClass('open');
-    $('.menu-overlay').toggleClass('open');
-  });
-});
